2e0ac4e098f8fb03c940ff2c3863c561c3d49403
Matej Skalicky <matej.skalickyy@gmail.com>
2026-07-22 20:29:22 +0200

    removed interface

--- coma.go
+++ coma.go
@@ -8,31 +8,11 @@ import (
 	"sync/atomic"
 )
 
-// ConcurrencyManager limits how many goroutines can run concurrently
-type ConcurrencyManager interface {
-	// Wait blocks until a slot is available and claims it for a new goroutine.
-	// In case WaitAllDone has been called, Wait returns ErrClosed.
-	Wait() error
-
-	// WaitContext blocks until a slot is available and claims it for a new goroutine,
-	// or returns ctx.Err() if the context is done first. If WaitAllDone has been called WaitContext returns ErrClosed.
-	WaitContext(context.Context) error
-
-	// Done marks a goroutine as finished and releases one slot.
-	Done()
-
-	// WaitAllDone waits until all goroutines are done. WaitAllDone is terminal, meaning ConcurrencyManager
-	// cannot be reused. It must be called at most once, from a single goroutine, a second call panics.
-	WaitAllDone()
-
-	// RunningCount returns the number of currently running goroutines.
-	RunningCount() int32
-}
-
 // ErrClosed is returned by Wait and WaitContext when WaitAllDone has been called.
 var ErrClosed = errors.New("coma: manager is shut down")
 
-type coMa struct {
+// ConcurrencyManager limits how many goroutines can run concurrently
+type ConcurrencyManager struct {
 	sem        chan struct{}
 	closed     chan struct{}
 	max        int32
@@ -40,8 +20,8 @@ type coMa struct {
 }
 
 // New creates a ConcurrencyManager that allows at most max concurrently running goroutines.
-func New(max int32) ConcurrencyManager {
-	return &coMa{
+func New(max int32) *ConcurrencyManager {
+	return &ConcurrencyManager{
 		sem:        make(chan struct{}, max),
 		closed:     make(chan struct{}),
 		max:        max,
@@ -49,7 +29,9 @@ func New(max int32) ConcurrencyManager {
 	}
 }
 
-func (c *coMa) Wait() error {
+// Wait blocks until a slot is available and claims it for a new goroutine.
+// In case WaitAllDone has been called, Wait returns ErrClosed.
+func (c *ConcurrencyManager) Wait() error {
 	select {
 	case c.sem <- struct{}{}:
 		c.runningCnt.Add(1)
@@ -59,7 +41,9 @@ func (c *coMa) Wait() error {
 	}
 }
 
-func (c *coMa) WaitContext(ctx context.Context) error {
+// WaitContext blocks until a slot is available and claims it for a new goroutine,
+// or returns ctx.Err() if the context is done first. If WaitAllDone has been called WaitContext returns ErrClosed.
+func (c *ConcurrencyManager) WaitContext(ctx context.Context) error {
 	select {
 	case c.sem <- struct{}{}:
 		c.runningCnt.Add(1)
@@ -71,18 +55,22 @@ func (c *coMa) WaitContext(ctx context.Context) error {
 	}
 }
 
-func (c *coMa) Done() {
+// Done marks a goroutine as finished and releases one slot.
+func (c *ConcurrencyManager) Done() {
 	<-c.sem
 	c.runningCnt.Add(-1)
 }
 
-func (c *coMa) WaitAllDone() {
+// WaitAllDone waits until all goroutines are done. WaitAllDone is terminal, meaning ConcurrencyManager
+// cannot be reused. It must be called at most once, from a single goroutine, a second call panics.
+func (c *ConcurrencyManager) WaitAllDone() {
 	close(c.closed)
 	for range c.max {
 		c.sem <- struct{}{}
 	}
 }
 
-func (c *coMa) RunningCount() int32 {
+// RunningCount returns the number of currently running goroutines.
+func (c *ConcurrencyManager) RunningCount() int32 {
 	return c.runningCnt.Load()
 }
