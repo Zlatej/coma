f2f6113026ae942c7277486a4480400d1753d419
Matej Skalicky <matej.skalickyy@gmail.com>
2026-08-02 22:54:44 +0200

    AcquireContext test

--- coma_test.go
+++ coma_test.go
@@ -1,6 +1,8 @@
 package coma
 
 import (
+	"context"
+	"errors"
 	"testing"
 	"time"
 )
@@ -17,29 +19,73 @@ func TestAcquire(t *testing.T) {
 		acqErr <- cm.Acquire()
 	}()
 
+	// blocked
 	select {
 	case err := <-acqErr:
 		t.Errorf("Acquire returned instead of blocking, err=%v", err)
-	case <-time.After(300 * time.Microsecond):
+	case <-time.After(100 * time.Millisecond):
 		// expected
 	}
-
 	if actual := cm.RunningCount(); actual != limit {
 		t.Errorf("RunningCount=%d should be equal to limit=%d", actual, limit)
 	}
 
 	cm.Release()
 
+	// acquired
 	select {
 	case err := <-acqErr:
 		if err != nil {
 			t.Error("after Release Acquire returned error, should return nil")
 		}
-	case <-time.After(300 * time.Microsecond):
+	case <-time.After(time.Second):
 		t.Errorf("Acquire seems to be blocked even after a slot was released")
 	}
-
 	if actual := cm.RunningCount(); actual != limit {
 		t.Errorf("after Release RunningCount=%d should be equal to limit=%d", actual, limit)
+	}
+}
+
+func TestAcquireContext(t *testing.T) {
+	const limit = 3
+	cm := New(limit)
+	ctx, cancel := context.WithCancel(context.Background())
+	for range limit {
+		cm.Acquire()
+	}
+
+	acqErr := make(chan error)
+	go func() {
+		acqErr <- cm.AcquireContext(ctx)
+	}()
+
+	// blocked
+	select {
+	case err := <-acqErr:
+		t.Errorf("AcquireContext returned instead of blocking, err=%v", err)
+	case <-time.After(100 * time.Millisecond):
+		// expected
+	}
+	if actual := cm.RunningCount(); actual != limit {
+		t.Errorf("RunningCount=%d should be equal to limit=%d", actual, limit)
+	}
+
+	cancel()
+
+	// canceled
+	select {
+	case err := <-acqErr:
+		if err != nil {
+			if !errors.Is(err, context.Canceled) {
+				t.Error("AcquireContext returned is not expected canceled context error")
+			}
+		} else {
+			t.Error("AcquireContext returned nil instead of canceled context error")
+		}
+	case <-time.After(time.Second):
+		t.Errorf("AcquireContext seems to be blocked even after canceling context")
+	}
+	if actual := cm.RunningCount(); actual != limit {
+		t.Errorf("RunningCount=%d should be equal to limit=%d", actual, limit)
 	}
 }
