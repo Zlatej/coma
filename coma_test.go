ce7f87fbd4a74b6686393fabb50cc958539d4550
Matej Skalicky <matej.skalickyy@gmail.com>
2026-08-05 09:03:29 +0200

    uhmmmmm ehmmmm

--- coma_test.go
+++ coma_test.go
@@ -3,12 +3,15 @@ package coma
 import (
 	"context"
 	"errors"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
+const limit = 3
+
 func TestAcquire(t *testing.T) {
-	const limit = 3
 	cm := New(limit)
 	for range limit {
 		cm.Acquire()
@@ -70,6 +73,24 @@ func TestAcquireContext(t *testing.T) {
 		t.Errorf("RunningCount=%d should be equal to limit=%d", actual, limit)
 	}
 
+	cm.Release()
+
+	// acquired
+	select {
+	case err := <-acqErr:
+		if err != nil {
+			t.Error("after Release AcquireContext returned error, should return nil")
+		}
+	case <-time.After(time.Second):
+		t.Errorf("AcquireContext seems to be blocked even after a slot was released")
+	}
+	if actual := cm.RunningCount(); actual != limit {
+		t.Errorf("after Release RunningCount=%d should be equal to limit=%d", actual, limit)
+	}
+
+	go func() {
+		acqErr <- cm.AcquireContext(ctx)
+	}()
 	cancel()
 
 	// canceled
@@ -86,6 +107,6 @@ func TestAcquireContext(t *testing.T) {
 		t.Errorf("AcquireContext seems to be blocked even after canceling context")
 	}
 	if actual := cm.RunningCount(); actual != limit {
-		t.Errorf("RunningCount=%d should be equal to limit=%d", actual, limit)
+		t.Errorf("RunningCount=%d after cancel should be equal to limit=%d", actual, limit)
 	}
 }
